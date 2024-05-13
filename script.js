7a9fbb1f1e5589373d1249c600dd464391d8d907
Anton <anton.zagarin@gmail.com>
2024-05-13 16:59:10 +0200

    created hw2

---
+++ script.js
@@ -0,0 +1,6 @@
+let str1 = prompt('Введите первую строку');
+let str2 = prompt('Введите вторую строку');
+let str3 = prompt('Введите третью строку');
+let arrOfString = [str1, str2, str3];
+
+console.log(arrOfString[Math.floor(Math.random()*3)]);
